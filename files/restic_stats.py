8d5f5ef941e3a2f8f210d619abec6df644ff890a
Marek Sirovy <msirovy@gmail.com>
2021-08-01 11:51:11 +0200

    stats in one script

--- files/restic_stats.py
+++ files/restic_stats.py
@@ -37,36 +37,27 @@ def restic_get_summary(file_path):
 
 
 if __name__ == "__main__":
-    job_name = argv[1]
-    file_age = file_age_in_min(argv[2])
+    stats_file = argv[1:]
+    for f in stats_file:
 
-    # if the source file is newer than 3O minutes regenerate snapshot stats
-    # if file_age < 30:
-    data = restic_get_summary(argv[2])
-    if data != {}:
-        print(f"""# Restic last snapshot stats
-# TYPE restic_stats_backup_duration gauge
+        job_name = path.basename(f).split(".")[0].replace("summary-","")
+        file_age = file_age_in_min(f)
+        data = restic_get_summary(f)
+        if data != {}:
+            print(f"""
 restic_stats_backup_duration{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['total_duration']}
-# TYPE restic_stats_data_added gauge
 restic_stats_data_added{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['data_added']}
-# TYPE restic_stats_bytes_processed gauge
 restic_stats_bytes_processed{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['total_bytes_processed']}
-# TYPE restic_stats_files_unmodified gauge
 restic_stats_files_unmodified{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['files_unmodified']}
-# TYPE restic_stats_files_changed gauge
 restic_stats_files_changed{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['files_changed']}
-# TYPE restic_stats_files_new gauge
 restic_stats_files_new{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['files_new']}
-# TYPE restic_stats_dirs_unmodified gauge
 restic_stats_dirs_unmodified{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['dirs_unmodified']}
-# TYPE restic_stats_dirs_changed gauge
 restic_stats_dirs_changed{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['dirs_changed']}
-# TYPE restic_stats_dirs_new gauge
 restic_stats_dirs_new{{backup_job="{job_name}", snapshot_id="{data['snapshot_id']}"}} {data['dirs_new']}
-        """)
+""")
 
-    print(f"""# Restic general stats
 # HELP restic_stats_last_snapshot_age Age of latest snapshot in minutes
-# TYPE restic_stats_last_snapshot_age counter
+
+            print(f"""
 restic_stats_last_snapshot_age{{backup_job="{job_name}"}} {file_age}
 """)
